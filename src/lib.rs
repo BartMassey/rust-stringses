a924efe5943853252683e7837f4c3fa258cbced0
Bart Massey <bart.massey@gmail.com>
2025-01-23 10:13:38 -0800

    cleaned up some types with traits

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-use std::marker::PhantomData;
-
 use thiserror::Error;
 
 #[derive(Debug, Error)]
@@ -8,38 +6,48 @@ pub enum StringerError {
     FailedConversion,
 }
 
-pub struct Stringer<G, T> {
-    letters: Vec<G>,
-    p: PhantomData<T>,
+pub trait Stringses {
+    type Letter;
 }
 
-impl<G, T> Default for Stringer<G, T> {
-    fn default() -> Self {
-        Self { letters: Vec::new(), p: PhantomData }
-    }
+pub struct Stringer<T: Stringses> {
+    letters: Vec<T::Letter>,
 }
 
 pub struct Asciies;
-//pub struct Byteses;
-pub struct Utf8ses;
-//pub struct Charses;
 
-impl<G: Default, T> Stringer<G, T> {
+impl Stringses for Asciies {
+    type Letter = u8;
+}
+
+pub struct Utf8ses;
+
+impl Stringses for Utf8ses {
+    type Letter = u8;
+}
+
+impl<T: Stringses> Default for Stringer<T> {
+    fn default() -> Self {
+        Self { letters: Vec::new() }
+    }
+}
+
+impl<T: Stringses> Stringer<T> {
     pub fn new() -> Self {
         Self::default()
     }
 }
 
-impl TryFrom<Stringer<u8, Utf8ses>> for Stringer<u8, Asciies> {
+impl TryFrom<Stringer<Utf8ses>> for Stringer<Asciies> {
     type Error = StringerError;
 
-    fn try_from(value: Stringer<u8, Utf8ses>) -> Result<Self, Self::Error> {
+    fn try_from(value: Stringer<Utf8ses>) -> Result<Self, Self::Error> {
         for &b in &value.letters {
             if b > 127 {
                 return Err(StringerError::FailedConversion);
             }
         }
         // XXX This could just be a transmute.
-        Ok(Self { letters: value.letters, p: PhantomData })
+        Ok(Self { letters: value.letters })
     }
 }
